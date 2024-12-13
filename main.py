96c7010b1d460741f84041b3f1bcd38a47b810ce
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-12-13 17:39:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,34 @@ import uvicorn
 from fastapi import FastAPI, Security
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_azure_auth import SingleTenantAzureAuthorizationCodeBearer
-from pydantic import AnyHttpUrl
+from pydantic import AnyHttpUrl, Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    BACKEND_CORS_ORIGINS: list[str | AnyHttpUrl] = ['http://localhost:8000']
-    OPENAPI_CLIENT_ID: str = "7a6732b1-565d-46e9-a080-e4003dfdc965"
-    APP_CLIENT_ID: str = "272f31f4-76fb-4641-816b-ce43703522ce"
-    TENANT_ID: str = "758eba73-8a49-40b5-915d-0085acccfcca"
-    SCOPE_DESCRIPTION: str = "user_impersonation"
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = Field(
+        default=['http://localhost:8000'],
+        env="BACKEND_CORS_ORIGINS"
+    )
+    OPENAPI_CLIENT_ID: str = Field(..., env="OPENAPI_CLIENT_ID")
+    APP_CLIENT_ID: str = Field(..., env="APP_CLIENT_ID")
+    TENANT_ID: str = Field(..., env="TENANT_ID")
+    SCOPE_DESCRIPTION: str = Field(default="user_impersonation", env="SCOPE_DESCRIPTION")
 
-    
     @property
     def SCOPE_NAME(self) -> str:
         return f'api://{self.APP_CLIENT_ID}/{self.SCOPE_DESCRIPTION}'
 
-    
     @property
     def SCOPES(self) -> dict:
         return {
             self.SCOPE_NAME: self.SCOPE_DESCRIPTION,
         }
 
-    
     @property
     def OPENAPI_AUTHORIZATION_URL(self) -> str:
         return f"https://login.microsoftonline.com/{self.TENANT_ID}/oauth2/v2.0/authorize"
 
-    
     @property
     def OPENAPI_TOKEN_URL(self) -> str:
         return f"https://login.microsoftonline.com/{self.TENANT_ID}/oauth2/v2.0/token"
@@ -39,6 +38,7 @@ class Settings(BaseSettings):
         env_file = '.env'
         env_file_encoding = 'utf-8'
         case_sensitive = True
+
 
 settings = Settings()
 
@@ -60,12 +60,12 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=['*'],
     )
 
-
 azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
     app_client_id=settings.APP_CLIENT_ID,
     tenant_id=settings.TENANT_ID,
     scopes=settings.SCOPES,
 )
+
 
 @app.on_event('startup')
 async def load_config() -> None:
@@ -73,6 +73,7 @@ async def load_config() -> None:
     Load OpenID config on startup.
     """
     await azure_scheme.openid_config.load_config()
+
 
 @app.get("/", dependencies=[Security(azure_scheme)])
 async def root():
